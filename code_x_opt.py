bf5b2b1368f3d49d8b46152611fd3965a694dcd2
EndearingUnion <127321280+EndearingUnion@users.noreply.github.com>
2026-06-04 11:50:07 +0200

    Update code_x_opt.py
    Nu met varience and noise level

--- code_x_opt.py
+++ code_x_opt.py
@@ -41,6 +41,13 @@ with h5py.File("source_atm_plus_photon.h5", "r") as f:
     times_atm_plus_phton_source = f["OBSATTRS"]["times"][...] 
 f.close()
 
+with h5py.File("blank_atm_plus_photon.h5", "r") as f:  
+    data_atm_plus_photon_blank = f["SPAXEL0"]["data"][...]
+    frequencies_atm_plus_photon_blank = f["OBSATTRS"]["frequencies"][...]
+    times_atm_plus_phton_blank = f["OBSATTRS"]["times"][...]
+f.close()
+
+
 
 eta_atm = np.load("eta_atmosphere.npy") #Load eta_atm
 tls_noise_parameters = np.load("tls_wls_params_alg.npy")
@@ -54,11 +61,15 @@ num_channels = data_atm_blank.shape[0]
 num_times = len(times_atm_blank)
 dt = np.mean(np.diff(times_atm_plus_phton_source)) 
 
+photon_noise = data_atm_plus_photon_blank  - data_atm_blank
+
 
 #data_complete = data_atm_plus_photon_source + data_tls_blank    #T_meas, contains signal from the source plus TLS, photon and atmospheric noise
 #data_source_tls_photon = data_atm_plus_photon_source + data_tls_blank - data_atm_blank
 
 x_opt_channels = np.zeros(num_channels)
+variance_channels = np.zeros(num_channels)
+noise_level_channels = np.zeros(num_channels)
 
 print("Shape eta_atm",eta_atm.shape)
 print("Shape freq source",frequencies_source.shape)
@@ -73,12 +84,12 @@ for channel in range(300):
     
     
     
-    #Data model \vec{y} = \vec{a}x + \mathbf{n}
+    #Data model \vec{y} = \vec{a}x + \mathbf{n}     
     #T_meas = data_complete[channel, :]      #Measured temperature
-    a_vec = eta_atm[channel, :]             #Atmoshpere transmission coefficient
+    a_vec = eta_atm[channel, :]          #Atmoshpere transmission coefficient
     
     #y_vec = data_complete[channel, :] - data_atm_blank[channel, :]#T_meas - (1 - a_vec)*T_P_ATM    #y vec containing the source signal, photon and TLS noise
-    y_vec = a_vec * temp_source[channel]  #AANPASSEN NAAR vec_a times x (gt)
+    y_vec = a_vec * temp_source[channel]  + data_tls_blank[channel, :] + photon_noise[channel, :]     #AANPASSEN NAAR vec_a times x (gt)
         
     freqs = fftfreq(num_times, d=dt)
     S_nn = np.zeros_like(freqs)
@@ -97,6 +108,14 @@ for channel in range(300):
     x_opt = np.dot(a_vec_tilde, y_vec_tilde) / np.dot(a_vec_tilde, a_vec_tilde) #x optimal
     
     x_opt_channels[channel] = x_opt
+
+
+    #varience = 1/sum(a_tile)
+    sum_a_vec_tilde = np.sum(a_vec_tilde**2)
+    variance_channels[channel] = (1/ sum_a_vec_tilde)
+    noise_level_channels[channel] = np.sqrt(variance_channels[channel])
     
     np.save("x_optimal.npy", x_opt_channels )
+    np.save("varience_channels.npy", variance_channels)
+    np.save("noise_level_channels", noise_level_channels)
 
